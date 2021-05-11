c52f46dec15b8a77116701ddb5f05c8610d7aff4
Sanaa Syed <sanaa.syed@shopify.com>
2021-05-11 13:04:50 -0400

    adding showcase section base

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import Header from './Header';
+import Showcase from './Showcase';
 
 function App() {
   return (
     <div>
       <Header />
       <hr />
+      <Showcase />
     </div>
   );
 }
--- src/Showcase.tsx
+++ src/Showcase.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import styles from './styles/Showcase.module.css';
+import { VerticalTimeline, VerticalTimelineElement }  from 'react-vertical-timeline-component';
+
 
 const Showcase = () => {
   return (
