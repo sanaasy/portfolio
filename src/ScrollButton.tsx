ccc6f7f4616c525410710e44b6459f96ae2643f8
Sanaa Syed <sanaa.syed@shopify.com>
2021-07-16 14:24:34 -0400

    adding icons to the scroll buttons

--- src/ScrollButton.tsx
+++ src/ScrollButton.tsx
@@ -1,5 +1,10 @@
 import React, {useState} from 'react';
 import styles from './styles/ScrollButton.module.css'
+import {
+  github,
+  linkedin,
+  medium,
+} from './assets';
   
 const ScrollButton = () =>{
   
@@ -25,9 +30,46 @@ const ScrollButton = () =>{
   window.addEventListener('scroll', toggleVisible);
   if(visible){
     return (
-      <button onClick={scrollToTop} style={{display: visible ? 'inline' : 'none'}} className={styles.Button}>
-        <div className={styles.Text}>back to the top ⬆️</div>
-      </button>
+      <span>
+        <button onClick={scrollToTop} style={{display: visible ? 'inline' : 'none'}} className={styles.Button}>
+          <div className={styles.Text}>back to the top ⬆️</div>
+        </button>
+
+        <div>
+        <ul className={styles.Link}>
+            <li className={styles.Social}>
+              <a
+                href="https://www.linkedin.com/in/sanaasy/"
+                target="_blank"
+                rel="noreferrer"
+                aria-label="Linkedin"
+              >
+                <img src={linkedin} alt="linkedin"/>
+              </a>
+            </li>
+            <li className={styles.Social}>
+              <a
+                href="https://www.github.com/sanaasy"
+                target="_blank"
+                rel="noreferrer"
+                aria-label="Github"
+              >
+                <img src={github} alt="github"/>
+              </a>
+            </li>
+            <li className={styles.Social}>
+              <a
+                href="https://medium.com/@sanaasyed"
+                target="_blank"
+                rel="noreferrer"
+                aria-label="Medium"
+              >
+                <img src={medium} alt="medium"/>
+              </a>
+            </li>
+          </ul>
+        </div>
+      </span>
     )
   } else {
     return <div />
