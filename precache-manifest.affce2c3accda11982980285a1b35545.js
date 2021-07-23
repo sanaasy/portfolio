6caa2c2aff14d4e23c84369b6a957a8195efa7a4
Sanaa Syed <sanaa.syed@shopify.com>
2021-07-23 11:16:00 -0400

    Updates

---
+++ precache-manifest.affce2c3accda11982980285a1b35545.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = [
+  {
+    "revision": "08c8b0b399b57a06348b",
+    "url": "/sanaasy.github.io/static/css/main.1549f39c.chunk.css"
+  },
+  {
+    "revision": "08c8b0b399b57a06348b",
+    "url": "/sanaasy.github.io/static/js/main.fcd85ef0.chunk.js"
+  },
+  {
+    "revision": "27e6bf1467192356bc4c",
+    "url": "/sanaasy.github.io/static/js/runtime~main.e4724932.js"
+  },
+  {
+    "revision": "6c3202d5177f653dc41e",
+    "url": "/sanaasy.github.io/static/js/2.e99b98e7.chunk.js"
+  },
+  {
+    "revision": "d5fd6816333719588409",
+    "url": "/sanaasy.github.io/static/js/3.64281f09.chunk.js"
+  },
+  {
+    "revision": "5bc01e5339a2fbb204b1fc6afdd20364",
+    "url": "/sanaasy.github.io/static/media/email.5bc01e53.png"
+  },
+  {
+    "revision": "8e292147cc1766406ceae245c2665f40",
+    "url": "/sanaasy.github.io/static/media/github.8e292147.png"
+  },
+  {
+    "revision": "1c9aef315be4e466284dcd21e945c543",
+    "url": "/sanaasy.github.io/static/media/linkedin.1c9aef31.png"
+  },
+  {
+    "revision": "060f8ee86180717a0a3a9abec31d5db9",
+    "url": "/sanaasy.github.io/static/media/medium.060f8ee8.png"
+  },
+  {
+    "revision": "bdbc3fc07f17095995e8a731cf3bfe78",
+    "url": "/sanaasy.github.io/static/media/cv.bdbc3fc0.png"
+  },
+  {
+    "revision": "d40185e593994329e3186b314f06a61a",
+    "url": "/sanaasy.github.io/static/media/logo.d40185e5.png"
+  },
+  {
+    "revision": "e97c68e6d9ee0362a814928f9db38144",
+    "url": "/sanaasy.github.io/static/media/sanaa.e97c68e6.png"
+  },
+  {
+    "revision": "0744ad77ce57103ee02eba0c792f6aad",
+    "url": "/sanaasy.github.io/index.html"
+  }
+];
